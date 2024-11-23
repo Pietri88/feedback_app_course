13aecfea2f7508777f5eef2370ebdf34abcee0aa
pietri88 <karol.pietras2@gmail.com>
2024-11-23 10:04:54 -0500

    trzeci commit

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -10,12 +10,44 @@ const FeedbackForm = () => {
 
     });
 
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const confirmationMessage = `
+        Imię: ${formData.name}
+        Email: ${formData.email}
+        Opinie: ${formData.feedback}
+        
+        `;
+    
+        const isConfirmed = window.confirm(`Proszę potwierdzić swoje dane:\n\n${confirmationMessage}`);
+    if (isConfirmed)
+    {
+        console.log('Wysyłanie opinii:', formData);
+        setFormData({
+            name:'',
+            email: '',
+            feedback: '',
+        });
+        alert('Dziękujemy za cenną opinię');
+
+    }
+    
+    };
+
   return (
     <>
     <nav>
     Tell Us What You Think
     </nav>
-      <form className="feedback-form">
+      <form onSubmit={handleSubmit} className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         
@@ -24,6 +56,8 @@ const FeedbackForm = () => {
             type="text"
             name="name"
             placeholder="Twoje Imię"
+            value={formData.name}
+            onChange={handleChange}
             />
 
             <input 
@@ -31,11 +65,15 @@ const FeedbackForm = () => {
             type="email"
             name="email"
             placeholder="Twój E-mail"
+            value={formData.email}
+            onChange={handleChange}
             />
 
             <textarea 
             name="feedback"
             placeholder="Twoja Opinia"
+            value={formData.feedback}
+            onChange={handleChange}
             ></textarea>
             <button type="submit">Wyślij Opinię</button>
 
