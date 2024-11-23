0cab870fc469d0f3ea433813af43702f02dca242
pietri88 <karol.pietras2@gmail.com>
2024-11-23 09:31:41 -0500

    drugi commit

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -3,6 +3,13 @@ import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
 
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        feedback: '',
+
+    });
+
   return (
     <>
     <nav>
@@ -12,6 +19,27 @@ const FeedbackForm = () => {
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         
+        <input 
+
+            type="text"
+            name="name"
+            placeholder="Twoje Imię"
+            />
+
+            <input 
+
+            type="email"
+            name="email"
+            placeholder="Twój E-mail"
+            />
+
+            <textarea 
+            name="feedback"
+            placeholder="Twoja Opinia"
+            ></textarea>
+            <button type="submit">Wyślij Opinię</button>
+
+
       </form>
     </>
   );
